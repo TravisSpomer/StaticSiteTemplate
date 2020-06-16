a8812e59780a11c9dc8aca3a8ffcc9ab3ee1983e
Travis Spomer <travis+github@greeneclipsesoftware.com>
2020-06-15 22:03:58 -0700

    Initial version

---
+++ Readme.md
@@ -0,0 +1,163 @@
+# Travis's static site template
+
+This project contains the basic site template that I use. I've set it up for rapid development and to produce simple, very-high-performance static websites the way that Grandma used to, but with a few extra modern necessities like TypeScript and CSS preprocessing. It has no particular runtime dependencies.
+
+This is a project for my own use and I don't have any plans to elevate it beyond that but you're welcome to take it and build on it from there. (Unless you prefer indenting with spaces, in which case GTFO.)
+
+## What you can do with it
+
+Back in the olden days, a website that didn't need server-side processing was just a folder full of files. I wanted to get as close to that experience as possible, while still being able to take advantage of a few key advancements in web technologies that have happened since then. But most notably, I *don't* want to use any sort of JavaScript-based UI framework like React.
+
+*	You can create one or more page layouts (templates) for your site to hold your common navigation and page structure.
+*	You can create pages in the site by just adding an HTML or Markdown file to the source tree.
+	*	There's intentionally no support for server-side code, but you can run compile-time code in your pages by creating a Handlebars "helper".
+*	You can write your client-side script in TypeScript instead of JavaScript, and it'll be automatically converted.
+*	Your code will be automatically linted and reformatted, ensuring consistent style and eliminating some common mistakes.
+*	You can include NPM packages without bundling.
+*	You can write CSS using the more modern SCSS (Sass) syntax, and it'll be automatically converted.
+*	The site template and styles include a few opinionated defaults for making beautiful text.
+*	A fast development server is included so you can preview your site instantly, and for production builds, the output is minified for maximum performance.
+
+For more details, see "Using the template" below.
+
+## Environment setup
+
+### Required software
+
+Download and install the latest stable versions of these products from their websites.
+
+1.	[Visual Studio Code](https://code.visualstudio.com)
+2.	[Node](https://nodejs.org/en/)
+3.	[Yarn](https://yarnpkg.com/lang/en/)
+	* **Important:** Install the "Classic" (v1) version of Yarn. Yarn v2 is an entirely different product and does not seem to be very compatible with Visual Studio Code and a few other parts of the ecosystem yet.
+
+In addition, in Windows, you must enable Developer Mode so that the site template can create symlinks without requiring administrator elevation.
+
+1.	Start > `developer settings`
+2.	Developer Mode > On
+
+### Building and running
+
+Whenever picking up a new version of the template, you need to make sure your dependencies are installed.  In Visual Studio Code:
+
+1.	Terminal > Run Task
+2.	Tasks: Run Task
+3.	Install dependencies
+
+Then, you can do a full dev build of the project with:
+
+1.	Ctrl+Shift+B (or Terminal > Run Build Task)
+
+But, the most useful option will be the **Start** task, which will perform a full dev build of the project, and then start a web server, open a tab in your browser, and automatically reload the site whenever you change anything while the server is running. The task uses [Browsersync](https://browsersync.io/), so you can open multiple browsers to the same site and they'll stay in sync.
+
+1.	Terminal > Run Task
+2.	Tasks: Run Task
+3.	Start
+
+To see the status of the server or build task, click the tools icon in the status bar, or Terminal > Show Running Tasks.
+
+When you're ready to produce a minified production build of the site, run the `prod` build command:
+
+1.	Terminal > Run Task
+2.	Tasks: Run Task
+3.	Production build
+
+#### Without Visual Studio Code
+
+If you want to use the project without Visual Studio Code, use the following scripts instead of the above:
+
+*	`yarn install`
+*	`yarn build`
+*	`yarn start`
+*	`yarn prod`
+
+## Using the template
+
+Here are some things to know about using this site template.
+
+### Customizing for your site
+
+At minimum, do this to customize the site for your purposes:
+
+1.	Set your site's deployed/canonical URL in [`staticsite.json`](staticsite.json)
+2.	Find and replace `StaticSiteTemplate` with the name of your site
+3.	Update the copyright information in ``src/layouts/*.hbs`](src/layouts/*.hbs)
+4.	Replace image assets in `src/images/app` with appropriate logos for your site
+5.	Add appropriate information to the [app manifest](src/app.webmanifest)
+6.	Give yourself credit in [`humans.txt`](src/humans.txt)
+
+#### Editing the page layout (template)
+
+This site template comes with two page layouts:
+
+*	[`default.hbs`](src/layouts/default.hbs), which is suitable for most pages and is used whenever a layout is not explicitly chosen by a page.
+*	[`plain.hbs`](src/layouts/plain.hbs), which is useful for popup windows and very custom pages that don't want the standard page contents.
+
+You can add any number of additional page layouts just by adding more files to that folder. Use `file.frontMatter` in your layout to access the page's FrontMatter data.
+
+There's also [`base.hbs`](src/layouts/base.hbs), which defines the page skeleton shared by both `default` and `plain`. (Currently it's a special case, but
+you could adapt the template to handle more than one base layout: see the call to `Handlebars.registerPartial` in [`gulpfile.js`](gulpfile.js).)
+
+### Compatibility
+
+Compatibility with Internet Explorer, "Spartan" (pre-Chromium) Edge, and other non-modern browsers is not a goal.
+
+### Site options
+
+Open [`staticsite.json`](staticsite.json) to configure options for the site.
+
+*	`azureStaticWebApps`: Set to `true` if deploying this site to Azure Static Web Apps, and `false` otherwise. Default is `false`.
+*	`canonicalUrl`: **Required.** Set this to the canonical deployed URL of the site. For example: `"https://www.example.com/"`.
+*	`defaultLayout`: Set to the name of the default layout template in `src/layouts`, without the extension. Default is `default`.
+*	`devServerPort`: The port to use when running the `serve` command. If not specified, browsersync will choose a port for you, likely 3000.
+*	`outputFolder`: Set to the name of the folder where you'd like the built site files to be created. Default is `build/`. The contents of this folder can be uploaded to your hosting provider as-is. **Important:** All files in this folder will be deleted when the output is built or cleaned.
+
+### Stuff you can safely delete
+
+The template includes a few files that aren't necessary that you can feel free to delete.
+
+*	`*.md` are just documentation.
+*	`site.ts` is the TypeScript source for the site. There's nothing in it, so it'll generate an empty JavaScript file. You don't need this if you aren't writing any site-wide client script.
+*	`humans.txt` is just for your benefit and is not necessary in any way.
+*	The `plain.hbs` layout isn't used by anything, and you may not need it.
+*	`lit-element` in `webDependencies` is only used as an example of how to include lightweight NPM packages for use in client-side script. You can remove from `webDependencies`.
+
+### Creating routes and redirects
+
+Use [`routes.json`](src/routes.json) to configure routes and redirects for the app.
+
+When the `azureStaticWebApps` option is set to true, this file will be copied as-is to the output folder and [used by the Azure Static Web Apps service](https://docs.microsoft.com/en-us/azure/static-web-apps/routes).
+
+When the option is set to false (or omitted), the pipeline will use its contents for two things:
+*	Redirects in the `routes` section will be created as redirect files. (In the example below, a file called `default.aspx` will be created, which redirects to `/`.)
+*	Files specified in `platformErrorOverrides` will not have their extensions stripped. (In the example below, `404.html` will *not* be output as `404/index.html` like it would normally.)
+
+#### Example routes.json
+
+```json
+{
+	"routes": [
+		{ "route": "default.aspx", "serve": "/", "statusCode": 301 }
+	],
+	"platformErrorOverrides": [
+		{ "errorType": "NotFound", "serve": "/404.html" }
+	]
+}
+```
+
+## Technologies used
+
+*	[Handlebars](https://www.handlebarsjs.com/) for page templating.
+*	[TypeScript](https://www.typescriptlang.org/) for JavaScript transpilation.
+*	[ESLint](https://eslint.org/) for JavaScript linting and formatting.
+*	[Terser](https://terser.org/) for JavaScript minification.
+*	[SCSS (Sass)](https://sass-lang.com/) for CSS preprocessing and minification.
+*	[Marked](https://marked.js.org/) for Markdown to HTML transformation.
+*	[HTML Minifier](https://github.com/kangax/html-minifier) for HTML minification.
+*	[Gulp](https://gulpjs.com/) for the build pipeline.
+*	[Snowpack](https://snowpack.dev/) for client package management.
+*	[Browsersync](https://www.browsersync.io/) for the development server.
+
+For more information, including licenses for these packages, check the `node_modules` subfolders.
+
+This template is © 2020 Travis Spomer but released to the public domain under the [CC0 1.0](https://creativecommons.org/publicdomain/zero/1.0) license. This license does not apply to sites built with this template, nor to external libraries referenced by this template; only the template itself.
---
+++ gulpfile.js
@@ -0,0 +1,380 @@
+const BrowserSync = require("browser-sync")
+const Del = require("del")
+const FrontMatter = require("gulp-front-matter")
+const FS = require("fs")
+const Gulp = require("gulp")
+const GulpHtmlMin = require("gulp-htmlmin")
+const GulpRename = require("gulp-rename")
+const Handlebars = require("handlebars")
+const Marked = require("gulp-marked")
+const Sass = require("gulp-sass")
+const Wrap = require("gulp-wrap")
+const Terser = require("gulp-terser")
+const TypeScript = require("gulp-typescript")
+// eslint-disable-next-line no-unused-vars
+const GulpDebug = require("gulp-debug")
+
+// ------------------------------------------------------------
+// Pipeline options: edit in staticsite.json
+// ------------------------------------------------------------
+
+// Check to make sure that the required pipeline options are set.
+const staticSiteJson = require("./staticsite.json")
+
+if (!("azureStaticWebApps" in staticSiteJson))
+	staticSiteJson.azureStaticWebApps = false
+if (!("canonicalUrl" in staticSiteJson))
+	throw new Error("The canonicalUrl setting must be specified in staticsite.json.")
+if (!("defaultLayout" in staticSiteJson))
+	staticSiteJson.defaultLayout = "default"
+if (!("outputFolder" in staticSiteJson))
+	staticSiteJson.outputFolder = "build/"
+else if (staticSiteJson.outputFolder.substring(staticSiteJson.outputFolder.length - 2, 1) !== "/")
+	staticSiteJson.outputFolder += "/"
+
+// ------------------------------------------------------------
+
+// ------------------------------------------------------------
+// Helper functions
+// ------------------------------------------------------------
+
+const templateGlob = ["src/layouts/*.hbs"]
+
+let cachedTemplates
+const getTemplate = (data) =>
+{
+	const layout = data.file.frontMatter.layout || staticSiteJson.defaultLayout
+	if (layout in cachedTemplates) return cachedTemplates[layout]
+
+	cachedTemplates[layout] = FS.readFileSync(`./src/layouts/${layout}.hbs`).toString()
+	return cachedTemplates[layout]
+}
+const clearCachedTemplates = () =>
+{
+	cachedTemplates = {}
+	Handlebars.unregisterPartial("base")
+	Handlebars.registerPartial("base", FS.readFileSync("src/layouts/base.hbs").toString())
+}
+clearCachedTemplates()
+
+const wrapInTemplate = () => Wrap(getTemplate, {}, { site: staticSiteJson, engine: "handlebars" })
+
+// Adds the current year, or a year range, to the template.
+// {{year}}  =>  "2020"
+// {{year from=1981}}  =>  "1981-2020"
+Handlebars.registerHelper("year", (options) =>
+{
+	const from = "hash" in options ? options.hash.from : undefined
+	const to = (new Date()).getUTCFullYear()
+	return (!from || to <= from) ? to.toString() : `${from}-${to}`
+})
+
+const routesJsonGlob = ["src/routes.json"]
+const routesJson = require(`./${routesJsonGlob[0]}`)
+
+// ------------------------------------------------------------
+// Tasks
+// ------------------------------------------------------------
+
+const clean = async (callback) =>
+{
+	await Del(`${staticSiteJson.outputFolder}**`)
+
+	callback()
+}
+clean.displayName = "Clean output folder"
+
+// ------------------------------------------------------------
+
+const terserOptions =
+{
+	keep_fnames: true,
+	mangle: false,
+	compress: {
+		ecma: 2015,
+		unsafe: true,
+		warnings: true,
+	},
+	output: {
+		comments: false,
+	}
+}
+
+const typescriptProject = TypeScript.createProject("tsconfig.json")
+const typescriptGlob = ["src/**/*.ts"]
+
+const typescript = (callback) =>
+{
+	const output = Gulp
+		.src(typescriptGlob)
+		.pipe(typescriptProject())
+		.pipe(Gulp.dest(staticSiteJson.outputFolder))
+
+	callback()
+	return output
+}
+typescript.displayName = "Compile TypeScript"
+
+const typescriptMin = (callback) =>
+{
+	const output = Gulp
+		.src(typescriptGlob)
+		.pipe(typescriptProject())
+		.pipe(Terser(terserOptions))
+		.pipe(Gulp.dest(staticSiteJson.outputFolder))
+
+	callback()
+	return output
+}
+typescriptMin.displayName = "Compile and minify TypeScript"
+
+// ------------------------------------------------------------
+
+const GulpHtmlMinOptions =
+{
+	collapseBooleanAttributes: true,
+	collapseWhitespace: true,
+	conservativeCollapse: true,
+	maxLineLength: 400,
+	minifyCSS: true,
+	minifyJS: true,
+	quoteCharacter: '"',
+	removeAttributeQuotes: true,
+	removeComments: true,
+	sortAttributes: true,
+}
+
+const pathsToNotRename = new Set(["./index.html"])
+for (const errorPage of routesJson.platformErrorOverrides)
+	pathsToNotRename.add(`.${errorPage.serve}`)
+
+const renameWithoutExtension = (path) =>
+{
+	if (pathsToNotRename.has(`${path.dirname}/${path.basename}${path.extname}`)) return path
+	return ({
+		...path,
+		dirname: `${path.dirname}/${path.basename}`,
+		basename: "index",
+		extname: ".html",
+	})
+}
+
+const htmlGlob = ["src/**/*.html", "src/**/*.htm"]
+
+const html = (callback) =>
+{
+	clearCachedTemplates()
+	const output = Gulp
+		.src(htmlGlob)
+		.pipe(GulpRename(renameWithoutExtension))
+		.pipe(FrontMatter())
+		.pipe(wrapInTemplate())
+		.pipe(Gulp.dest(staticSiteJson.outputFolder))
+
+	callback()
+	return output
+}
+html.displayName = "Process HTML pages"
+
+const htmlMin = (callback) =>
+{
+	clearCachedTemplates()
+	const output = Gulp
+		.src(htmlGlob)
+		.pipe(GulpRename(renameWithoutExtension))
+		.pipe(FrontMatter())
+		.pipe(wrapInTemplate())
+		.pipe(GulpHtmlMin(GulpHtmlMinOptions))
+		.pipe(Gulp.dest(staticSiteJson.outputFolder))
+
+	callback()
+	return output
+}
+htmlMin.displayName = "Process and minify HTML pages"
+
+// ------------------------------------------------------------
+
+const markdownGlob = ["src/**/*.md"]
+
+const markdown = (callback) =>
+{
+	clearCachedTemplates()
+	const output = Gulp
+		.src(markdownGlob)
+		.pipe(FrontMatter())
+		.pipe(Marked())
+		.pipe(wrapInTemplate())
+		.pipe(Gulp.dest(staticSiteJson.outputFolder))
+
+	callback()
+	return output
+}
+markdown.displayName = "Convert Markdown to HTML"
+
+const markdownMin = (callback) =>
+{
+	clearCachedTemplates()
+	const output = Gulp
+		.src(markdownGlob)
+		.pipe(FrontMatter())
+		.pipe(Marked())
+		.pipe(wrapInTemplate())
+		.pipe(GulpHtmlMin(GulpHtmlMinOptions))
+		.pipe(Gulp.dest(staticSiteJson.outputFolder))
+
+	callback()
+	return output
+}
+markdownMin.displayName = "Convert Markdown to HTML and minify"
+
+// ------------------------------------------------------------
+
+Sass.compiler = require("node-sass")
+
+const sassGlob = ["src/**/*.scss"]
+
+const css = (callback) =>
+{
+	const output = Gulp
+		.src(sassGlob)
+		.pipe(Sass({ outputStyle: "expanded" }).on("error", Sass.logError))
+		.pipe(Gulp.dest(staticSiteJson.outputFolder))
+
+	callback()
+	return output
+}
+css.displayName = "Compile SASS into CSS"
+
+const cssMin = (callback) =>
+{
+	const output = Gulp
+		.src(sassGlob)
+		.pipe(Sass({ outputStyle: "compressed" }).on("error", Sass.logError))
+		.pipe(Gulp.dest(staticSiteJson.outputFolder))
+
+	callback()
+	return output
+}
+cssMin.displayName = "Compile SASS into CSS and minify"
+
+// ------------------------------------------------------------
+
+const redirects = (callback) =>
+{
+	// Skip this for Azure Static Web apps, because that handles routes.json natively.
+	if (staticSiteJson.azureStaticWebApps)
+	{
+		callback()
+		return null
+	}
+
+	for (const routeData of routesJson.routes)
+	{
+		const contents = `<meta http-equiv=refresh content="0;url=${routeData.serve}"><link rel=canonical href="${staticSiteJson.canonicalUrl}${routeData.serve}">`
+
+		FS.writeFileSync(staticSiteJson.outputFolder + routeData.route, contents)
+	}
+
+	callback()
+	return null
+}
+redirects.displayName = "Generate redirects"
+
+// ------------------------------------------------------------
+
+const staticGlob = ["src/**", "!src", "!src/**/*.{ts,html,htm,md,scss,hbs}"]
+if (!staticSiteJson.azureStaticWebApps)
+	staticGlob.push("!src/routes.json")
+
+const symlink = (callback) =>
+{
+	const output = Gulp
+		.src(staticGlob, { nodir: true })
+		.pipe(Gulp.symlink(staticSiteJson.outputFolder, { relativeSymlinks: false }))
+
+	callback()
+	return output
+}
+symlink.displayName = "Symlink static files"
+
+// ------------------------------------------------------------
+
+const webModulesGlob = ["web_modules/**/*.js"]
+const webModulesOutputFolder = `${staticSiteJson.outputFolder}web_modules/`
+
+const webModules = (callback) =>
+{
+	const output = Gulp
+		.src(webModulesGlob)
+		.pipe(Gulp.symlink(webModulesOutputFolder, { relativeSymlinks: false, overwrite: true }))
+
+	callback()
+	return output
+}
+webModules.displayName = "Symlink web_modules"
+
+const webModulesMin = (callback) =>
+{
+	const output = Gulp
+		.src(webModulesGlob)
+		.pipe(Terser(terserOptions))
+		.pipe(Gulp.dest(webModulesOutputFolder))
+
+	callback()
+	return output
+}
+webModulesMin.displayName = "Copy and minify web_modules"
+
+// ------------------------------------------------------------
+
+const serve = (callback) =>
+{
+	const browserSyncOptions = require("./browsersync.json")
+	if (staticSiteJson.devServerPort && !browserSyncOptions.port)
+		browserSyncOptions.port = staticSiteJson.devServerPort
+	BrowserSync.init(browserSyncOptions)
+
+	callback()
+}
+serve.displayName = "Start a server"
+
+// ------------------------------------------------------------
+
+const setupOutput = (callback) =>
+{
+	if (!FS.existsSync(webModulesOutputFolder))
+		FS.mkdirSync(webModulesOutputFolder, { recursive: true })
+
+	callback()
+}
+setupOutput.displayName = "Set up output folder"
+
+// ------------------------------------------------------------
+// Exports
+// ------------------------------------------------------------
+
+const build = Gulp.parallel(typescript, html, markdown, css, webModules, redirects, symlink)
+
+const prod = Gulp.parallel(typescriptMin, htmlMin, markdownMin, cssMin, webModulesMin, redirects, symlink)
+
+const watch = (callback) =>
+{
+	Gulp.watch(typescriptGlob, typescript)
+	Gulp.watch([...htmlGlob, ...templateGlob], html)
+	Gulp.watch([...markdownGlob, ...templateGlob], markdown)
+	Gulp.watch(sassGlob, css)
+	Gulp.watch(routesJsonGlob, redirects)
+	// We do a full build before watching, so it's not necessary to do symlinking again here.
+
+	callback()
+}
+watch.displayName = "Watch for changes"
+
+exports.clean = Gulp.series(setupOutput, clean)
+exports.build = Gulp.series(setupOutput, clean, build)
+exports.prod = Gulp.series(setupOutput, clean, prod)
+exports.watch = Gulp.series(setupOutput, clean, build, watch)
+exports.serve = Gulp.series(setupOutput, serve)
+exports.start = Gulp.series(setupOutput, clean, build, Gulp.parallel(watch, serve))
+
+exports.default = exports.build
